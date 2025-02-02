cb4a5cc4c6c666f4149dbfb566f3db98ba97b52f
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-02-02 19:58:22 +0100

    update phpdoc

--- src/Action/PhpSandbox.php
+++ src/Action/PhpSandbox.php
@@ -1,9 +1,9 @@
 <?php
 /*
  * Fusio
- * A web-application to create dynamically RESTful APIs
+ * An open source API management platform which helps to create innovative API solutions
  *
- * Copyright (C) 2015-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
+ * Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
